59f114eb359ee35fc8c9798e19adb42df7378ee3
unknown <odinaka.dev@gmail.com>
2024-05-28 16:56:54 -0700

    updating: music, music image, favicon logo

--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
-import qala from "../assets/qala.mp3";
-// import musicImage from "../assets/music-player.jpg";
+import winMusic from "../assets/all-i-do-is-win.mp3";
+import musicImage from "../assets/music-player.jpg";
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 import { IconContext } from "react-icons";
@@ -19,7 +19,7 @@ export default function Player() {
 
   const [seconds, setSeconds] = useState();
 
-  const [play, { pause, duration, sound }] = useSound(qala);
+  const [play, { pause, duration, sound }] = useSound(winMusic);
 
   useEffect(() => {
     if (duration) {
@@ -61,10 +61,10 @@ export default function Player() {
   return (
     <div className="component">
       <h2>Playing Now</h2>
-      {/* <img className="musicCover" src={musicImage} /> */}
+      <img className="musicCover" src={musicImage} />
       <div>
-        <h3 className="title">Rubaiyyan</h3>
-        <p className="subTitle">Qala</p>
+        <h3 className="title">All I Do Is Win</h3>
+        <p className="subTitle">Rick Ross ft. DJ Khaled, Ludacris, Snoop Dogg & T Pain</p>
       </div>
       <div>
         <div className="time">
