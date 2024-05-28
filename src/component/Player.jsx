a064b8fd67938a588b9497670d92a2774ce0395d
Torh-21 <majarotoheeb@gmail.com>
2024-05-28 02:34:54 -0500

    App component fix

--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
-import qala from "../assets/qala.mp3";
+import qala from "../assets/ken.mp3";
 // import musicImage from "../assets/music-player.jpg";
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
